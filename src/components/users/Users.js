bbb59097195e5fe844b281612adb8de6bdc3072c
Nndou <yp.niu@foxmail.com>
2019-08-17 13:47:19 +1000

    add spinner components

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import UserItem from "./UserItem";
+import Spinner from "../layout/Spinner";
+import PropTypes from "prop-types";
 
-class Users extends Component {
-  render() {
+const Users = ({ users, loading }) => {
+  if (loading) {
+    return <Spinner />;
+  } else {
     return (
       <div className='py-3 row'>
-        {this.props.users.map(user => (
+        {users.map(user => (
           <div key={user.id} className='col-md-4 px-1'>
             <UserItem user={user} />
           </div>
@@ -13,6 +17,9 @@ class Users extends Component {
       </div>
     );
   }
-}
-
+};
+Users.PropTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired
+};
 export default Users;
