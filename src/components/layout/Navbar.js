5f5cee66a8d1340dd5904b001d43aec99db0e79d
Nndou <yp.niu@foxmail.com>
2019-08-18 12:27:46 +1000

    Feat:add About page

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,14 +1,29 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 const Navbar = ({ icon, title }) => {
   return (
-    <div className='navbar bg-info text-light'>
-      <h1 className='mb-0' style={{ fontSize: "30px" }}>
-        <i className={icon} />
-        {title}
-      </h1>
-    </div>
+    <nav className='navbar bg-info text-light navbar-expand'>
+      <div className='container-fluid'>
+        <h1 className='mb-0' style={{ fontSize: "30px" }}>
+          <i className={icon} />
+          {title}
+        </h1>
+        <ul className='navbar-nav ml-auto'>
+          <li className='nav-item'>
+            <Link className='nav-link text-light' to='/'>
+              Home
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link className='nav-link text-light' to='/about'>
+              About
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
