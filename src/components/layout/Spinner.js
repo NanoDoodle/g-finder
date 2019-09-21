92de61427542414d6511d44dcf7c481e6d4de8cf
Nndou <yp.niu@foxmail.com>
2019-09-21 18:47:00 +1000

    Add a loader

--- src/components/layout/Spinner.js
+++ src/components/layout/Spinner.js
@@ -1,14 +1,63 @@
 import React, { Fragment } from "react";
-import spinner from "./spinner.gif";
 
-const Spinner = () => (
-  <Fragment>
-    <img
-      src={spinner}
-      alt='loading...'
-      style={{ width: "200px", margin: "auto", display: "block" }}
-    />
-  </Fragment>
+const loader = (
+  <svg
+    width='80'
+    height='80'
+    viewBox='0 0 45 45'
+    xmlns='http://www.w3.org/2000/svg'
+    stroke='#404969'
+    style={{ margin: "auto", display: "block", marginTop: "20px" }}
+  >
+    <g fill='none' fill-rule='evenodd' stroke-width='2'>
+      <circle cx='22' cy='22' r='1'>
+        <animate
+          attributeName='r'
+          begin='0s'
+          dur='1.8s'
+          values='1; 20'
+          calcMode='spline'
+          keyTimes='0; 1'
+          keySplines='0.165, 0.84, 0.44, 1'
+          repeatCount='indefinite'
+        />
+        <animate
+          attributeName='stroke-opacity'
+          begin='0s'
+          dur='1.8s'
+          values='1; 0'
+          calcMode='spline'
+          keyTimes='0; 1'
+          keySplines='0.3, 0.61, 0.355, 1'
+          repeatCount='indefinite'
+        />
+      </circle>
+      <circle cx='22' cy='22' r='1'>
+        <animate
+          attributeName='r'
+          begin='-0.9s'
+          dur='1.8s'
+          values='1; 20'
+          calcMode='spline'
+          keyTimes='0; 1'
+          keySplines='0.165, 0.84, 0.44, 1'
+          repeatCount='indefinite'
+        />
+        <animate
+          attributeName='stroke-opacity'
+          begin='-0.9s'
+          dur='1.8s'
+          values='1; 0'
+          calcMode='spline'
+          keyTimes='0; 1'
+          keySplines='0.3, 0.61, 0.355, 1'
+          repeatCount='indefinite'
+        />
+      </circle>
+    </g>
+  </svg>
 );
+
+const Spinner = () => <Fragment>{loader}</Fragment>;
 
 export default Spinner;
