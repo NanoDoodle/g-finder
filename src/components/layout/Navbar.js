69c11be948aa4104d62741f1a7af33b2a1310e94
Nndou <yp.niu@foxmail.com>
2019-09-25 15:21:11 +1000

    Fix:clear searchState when click home btn

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
+import GithubContext from "../../context/github/githubContext";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
 const Navbar = ({ icon, title }) => {
+  const githubContext = useContext(GithubContext);
+
+  const { clearSearch } = githubContext;
+
   return (
     <nav className='navbar text-primary navbar-expand'>
       <div className='container'>
-        <Link className='nav-link text-primary' to='/'>
+        <Link className='nav-link text-primary' onClick={clearSearch} to='/'>
           <h1 className='mb-0'>
             <i className={icon} />
             {title}
@@ -15,7 +20,11 @@ const Navbar = ({ icon, title }) => {
 
         <ul className='navbar-nav ml-auto'>
           <li className='nav-item'>
-            <Link className='nav-link text-primary' to='/'>
+            <Link
+              className='nav-link text-primary'
+              onClick={clearSearch}
+              to='/'
+            >
               Home
             </Link>
           </li>
